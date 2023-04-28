a61b63abb6d2e07abf5f08d62578fefb5019fee8
DGubliauskas <122284934+DGubliauskas@users.noreply.github.com>
2023-04-28 20:21:14 +0300

    2023-04-28

---
+++ joke-on-you/src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss';
+import Jokes from './components/Jokes';
+
+function App() {
+  
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <h2>01000010 01100101 01110011 01110100 00100000 01100100 01100001 01100100 00100000 01101010 01101111 01101011 01100101 01110011</h2>
+        <Jokes />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
